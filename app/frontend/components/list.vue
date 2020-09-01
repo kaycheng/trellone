73f4c868d1fb3c613097fc7a5c26536845cf0455
Kaycheng <kaycheng717@gmail.com>
2020-09-01 19:44:44 +0800

    Add new card in list component

--- app/frontend/components/list.vue
+++ app/frontend/components/list.vue
@@ -2,18 +2,55 @@
   <div class="list">
     <h2 class="header">{{ list.name }}</h2>
     <div class="deck">
-      <Card v-for="card in list.cards" :card="card" :key="card.id"></Card>
+      <Card v-for="card in cards" :card="card" :key="card.id"></Card>
+    
+
+      <div class="input-area">
+        <textarea rows="3" class="content" v-model="content"></textarea>
+        <button class="button" @click="createCard">Create Card</button>
+        <button class="button">Cancel</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Rails from "@rails/ujs";
 import Card from "components/card";
 
 export default {
   name: "List",
   props: ["list"],
-  components: { Card }
+  components: { Card },
+  data: function() {
+    return {
+      content: '',
+      cards: this.list.cards
+    }
+  },
+  methods: {
+    createCard(event) {
+      event.preventDefault();
+
+      let data = new FormData();
+      data.append("card[list_id]", this.list.id);
+      data.append("card[name]", this.content);
+
+      Rails.ajax({
+        url: "/cards",
+        type: "POST",
+        data,
+        dataType: "json",
+        success: resp => {
+          this.cards.push(resp);
+          this.content = '';
+        },
+        error: err => {
+          console.log(err)
+        }
+      });
+    }
+  }
 }
 </script>  
 
@@ -27,6 +64,25 @@ export default {
 
   .deck {
     @apply .mt-2;
+  }
+
+  .input-area {
+    @apply .mt-2;
+    .content {
+      @apply .w-full .rounded .p-2;
+
+      &:focus {
+        @apply .outline-none;
+      }
+    }
+
+    .button {
+      @apply .bg-gray-400 .rounded-sm .px-3 .py-1 .text-sm .font-semibold;
+
+      &:focus {
+        @apply .outline-none;
+      }
+    }
   }
 }
 </style>
